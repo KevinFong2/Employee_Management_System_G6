a8f80a19da1c66c88b45f93aa9399107d9001afd
Jun Huang <jh123.jun@gmail.com>
2023-05-16 14:06:45 -0400

    Added the ability to add or remove tasks from an employee

--- src/redux/Tasks.js
+++ src/redux/Tasks.js
@@ -5,76 +5,93 @@ const path = "https://employee-management8.herokuapp.com";
 
 // Define the async thunk to fetch the tasks data
 export const fetchTasks = createAsyncThunk(
-'tasks/fetchTasks',
-async () => {
-    try {
-    const response = await axios.get(`${path}/tasks`);
-    return response.data;
-    } catch (err) {
-    throw err;
+    'tasks/fetchTasks',
+    async () => {
+        try {
+            const response = await axios.get(`${path}/tasks`);
+            return response.data;
+        } catch (err) {
+            throw err;
+        }
     }
-}
 );
 
 // Define the async thunk to add a task to the API
 export const addTask = createAsyncThunk(
-'tasks/addTask',
-async (task) => {
-    try {
-    const response = await axios.post(`${path}/tasks`, task);
-    return response.data;
-    } catch (err) {
-    throw err;
+    'tasks/addTask',
+    async (task) => {
+        try {
+            const response = await axios.post(`${path}/tasks`, task);
+            return response.data;
+        } catch (err) {
+            throw err;
+        }
     }
-}
 );
 
 // Define the async thunk to delete a task from the API
 export const deleteTask = createAsyncThunk(
-'tasks/deleteTask',
-async (id) => {
-    try {
-    await axios.delete(`${path}/tasks/${id}`);
-    return id;
-    } catch (err) {
-    throw err;
+    'tasks/deleteTask',
+    async (id) => {
+        try {
+            await axios.delete(`${path}/tasks/${id}`);
+            return id;
+        } catch (err) {
+            throw err;
+        }
     }
-}
 );
 
-export const getUnassignedTasksIds = (state) => {
-    return state.tasks.tasksData.filter((task) => !task.employeeId).map((task) => task.id);
-};  
+export const updateTask = createAsyncThunk(
+    'tasks/updateTask',
+    async (task) => {
+        try {
+            const response = await axios.put(`${path}/tasks/${task.id}`, task);
+            return response.data;
+        } catch (err) {
+            throw err;
+        }
+    }
+);
 
 const initialState = {
-tasksData: [],
-status: 'idle',
-error: null,
+    tasksData: [],
+    status: 'idle',
+    error: null,
 };
 
 export const tasksSlice = createSlice({
-name: 'tasks',
-initialState,
-reducers: {},
-extraReducers: (builder) => {
-    builder.addCase(fetchTasks.pending, (state) => {
-    state.status = 'loading';
-    });
-    builder.addCase(fetchTasks.fulfilled, (state, action) => {
-    state.status = 'succeeded';
-    state.tasksData = action.payload;
-    });
-    builder.addCase(fetchTasks.rejected, (state, action) => {
-    state.status = 'failed';
-    state.error = action.error.message;
-    });
-    builder.addCase(deleteTask.fulfilled, (state, action) => {
-    state.status = 'succeeded';
-    state.tasksData = state.tasksData.filter(
-        (task) => task.id !== action.payload
-    );
-    });
-},  
+    name: 'tasks',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchTasks.pending, (state) => {
+            state.status = 'loading';
+        });
+        builder.addCase(fetchTasks.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.tasksData = action.payload;
+        });
+        builder.addCase(fetchTasks.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+        });
+        builder.addCase(deleteTask.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.tasksData = state.tasksData.filter(
+                (task) => task.id !== action.payload
+            );
+        });
+        builder.addCase(updateTask.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            const index = state.tasksData.findIndex(task => task.id === action.payload.id);
+            if (index !== -1) {
+                state.tasksData[index] = action.payload;
+            }
+        });
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(fetchTasks),
 });
 
 // Export the async thunks and the reducer
--- src/redux/employees.js
+++ src/redux/employees.js
@@ -5,104 +5,106 @@ const path = "https://employee-management8.herokuapp.com";
 
 // Define the async thunk to fetch the employees data
 export const fetchEmployees = createAsyncThunk(
-  'employees/fetchEmployees',
-  async () => {
-    try {
-      const response = await axios.get(`${path}/employees`);
-      return response.data;
-    } catch (err) {
-      throw err;
+    'employees/fetchEmployees',
+    async () => {
+      try {
+        const response = await axios.get(`${path}/employees`);
+        return response.data;
+      } catch (err) {
+        throw err;
+      }
     }
-  }
 );
 
 // Define the async thunk to fetch a single employee data from the API
 export const fetchEmployee = createAsyncThunk(
-  'employees/fetchEmployee',
-  async (id) => {
-    try {
-      const response = await axios.get(`${path}/employees/${id}`);
-      return response.data;
-    } catch (err) {
-      throw err;
+    'employees/fetchEmployee',
+    async (id) => {
+      try {
+        const response = await axios.get(`${path}/employees/${id}`);
+        return response.data;
+      } catch (err) {
+        throw err;
+      }
     }
-  }
 );
 
 // Define the async thunk to add an employee to the API
 export const addEmployee = createAsyncThunk(
-  'employees/addEmployee',
-  async (employee) => {
-    try {
-      const response = await axios.post(`${path}/employees`, employee);
-      return response.data;
-    } catch (err) {
-      throw err;
+    'employees/addEmployee',
+    async (employee) => {
+      try {
+        const response = await axios.post(`${path}/employees`, employee);
+        return response.data;
+      } catch (err) {
+        throw err;
+      }
     }
-  }
 );
 
 // Define the async thunk to delete an employee from the API
 export const deleteEmployee = createAsyncThunk(
-  'employees/deleteEmployee',
-  async (id) => {
-    try {
-      await axios.delete(`${path}/employees/${id}`);
-      return id;
-    } catch (err) {
-      throw err;
+    'employees/deleteEmployee',
+    async (id) => {
+      try {
+        await axios.delete(`${path}/employees/${id}`);
+        return id;
+      } catch (err) {
+        throw err;
+      }
     }
-  }
 );
 
 export const updateEmployee = createAsyncThunk(
-  'employees/updateEmployee',
-  async (employee) => {
-    try {
-      const response = await axios.put(`${path}/employees/${employee.id}`, employee);
-      return response.data;
-    } catch (err) {
-      throw err;
+    'employees/updateEmployee',
+    async (employee) => {
+      try {
+        const response = await axios.put(`${path}/employees/${employee.id}`, employee);
+        return response.data;
+      } catch (err) {
+        throw err;
+      }
     }
-  }
 );
 
 const initialState = {
-  employeesData: [],
-  status: 'idle',
-  error: null,
+    employeesData: [],
+    status: 'idle',
+    error: null,
 };
 
 export const employeesSlice = createSlice({
-  name: 'employees',
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchEmployees.pending, (state) => {
-      state.status = 'loading';
-    });
-    builder.addCase(fetchEmployees.fulfilled, (state, action) => {
-      state.status = 'succeeded';
-      state.employeesData = action.payload;
-    });
-    builder.addCase(fetchEmployees.rejected, (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
-    });
-    builder.addCase(deleteEmployee.fulfilled, (state, action) => {
-      state.status = 'succeeded';
-      state.employeesData = state.employeesData.filter(
-        (employee) => employee.id !== action.payload
-      );
-    });
-    builder.addCase(updateEmployee.fulfilled, (state, action) => {
-      state.status = 'succeeded';
-      const index = state.employeesData.findIndex(employee => employee.id === action.payload.id);
-      if (index !== -1) {
-        state.employeesData[index] = action.payload;
-      }
-    });
-  },  
+    name: 'employees',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder.addCase(fetchEmployees.pending, (state) => {
+        state.status = 'loading';
+      });
+      builder.addCase(fetchEmployees.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.employeesData = action.payload;
+      });
+      builder.addCase(fetchEmployees.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+      builder.addCase(deleteEmployee.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.employeesData = state.employeesData.filter(
+          (employee) => employee.id !== action.payload
+        );
+      });
+      builder.addCase(updateEmployee.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        const index = state.employeesData.findIndex(employee => employee.id === action.payload.id);
+        if (index !== -1) {
+          state.employeesData[index] = action.payload;
+        }
+      });
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(fetchEmployees),
 });
 
 // Export the async thunks and the reducer
